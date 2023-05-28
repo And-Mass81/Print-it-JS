7b4a0d66a3db75a4d6dda76272156bfeed2be2a3
And-Mass81 <andmass10@gmail.com>
2023-05-28 23:06:20 +0200

    JS finished
    code + comments

--- assets/script.js
+++ assets/script.js
@@ -16,3 +16,39 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+let right = document.getElementById("arrow_right");
+let left = document.getElementById("arrow_left");
+let imageBanner = document.querySelector('#banner > img');
+let textLine = document.querySelector('#banner > p');
+let dots = document.querySelectorAll(".dot");
+let imagePlace =0;
+
+right.addEventListener('click', () => {
+     imagePlace++;
+     if (imagePlace > slides.length - 1) {
+          imagePlace = 0;
+     }
+     imageBanner.src = `./assets/images/slideshow/${slides[imagePlace].image}`;
+     // to be able to concatenate the string with variables i use `{$}` 
+     textLine.innerHTML = slides[imagePlace].tagLine;
+     // index of the dots , i use the counter of the imagePlace
+     dots.forEach(item => item.classList.remove('dot_selected'))
+     // i need to remove all the class dot_selected for each click 
+     dots[imagePlace].classList.add('dot_selected')
+     // to add only the class in the index of the image
+     console.log(imagePlace);
+});
+
+
+left.addEventListener('click', () => {
+     imagePlace = imagePlace - 1;
+     if (imagePlace < 0) {
+          imagePlace = slides.length - 1;
+     }
+	 imageBanner.src = `./assets/images/slideshow/${slides[imagePlace].image}`;
+     textLine.innerHTML = slides[imagePlace].tagLine;
+     dots.forEach(item => item.classList.remove('dot_selected'))
+     dots[imagePlace].classList.add('dot_selected')
+     console.log(imagePlace);
+});
